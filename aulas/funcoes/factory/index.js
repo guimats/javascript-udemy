3af0007c3c598fd0382222f6f0d1c4a11312b8a2
guimats <guilherme.mat.ds@gmail.com>
2024-08-01 14:30:43 -0300

    AULAS JS

---
+++ aulas/funcoes/factory/index.js
@@ -0,0 +1,45 @@
+// Factory funcrion (função fábrica)
+// this semelhante ao self (python)
+function criaPessoa(nome, sobrenome, alt, pes) {
+    return {
+        nome,
+        sobrenome,
+
+        // Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+        },
+
+        fala(assunto) {
+            return `${this.nome} está ${assunto}`
+        },
+        altura: alt,
+        peso: pes,
+
+        // Getter
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        },
+    };
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio', 1.80, 80);
+console.log(p1.nomeCompleto)
+p1.nomeCompleto = 'Maria Oliveira Silva'
+console.log(p1.nomeCompleto)
+
+
+// const p2 = criaPessoa('Maria', 'Joaquina', 1.62, 42);
+
+// console.log(p1.fala('falando sobre JS'));
+// console.log(p2.fala('falando sobre JS'));
+
+// console.log(p1.imc);
+// console.log(p2.imc);
