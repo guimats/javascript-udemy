8d50fc8d528b3f436e581829470d33eab81e5862
guimats <guilherme.mat.ds@gmail.com>
2024-08-22 12:22:39 -0300

    Aulas sobre Expres

---
+++ node/express_aula/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//         POST    GET   PUT     DELETE
+
+// http://meusite.com/ <- GET -> entregue a página /
+// http://meusite.com/sobre <- GET -> entregue a página /sobre
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulario!');
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato com a gente!');
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
