d8fa2be4a316c763cd9fcd02e612dd9cb748de30
guimats <guilherme.mat.ds@gmail.com>
2024-08-23 12:53:27 -0300

    Aulas express Node JS

--- node/express_aula_4/server.js
+++ node/express_aula_4/server.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('BD conectado')
+        app.emit('pronto');
+    })
+    .catch(e => console.log(e));
+
+
+
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal } = require('./src/middlewares/middleware');
@@ -14,7 +26,10 @@ app.set('view engine', 'ejs');
 app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000');
+    });
 });
+
